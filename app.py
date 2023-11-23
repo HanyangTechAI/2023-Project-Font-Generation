b1675cb58eaec6580014a158f6db0fc9dff33348
seoyeon-922 <tjdus092222@gmail.com>
2023-11-23 15:34:27 +0900

    add load_model

--- app.py
+++ app.py
@@ -4,69 +4,27 @@ import base64
 import requests
 from PIL import Image
 from os.path import isdir
-#SMTP
+# SMTP
 from email.message import EmailMessage
 from smtplib import SMTP_SSL
 from pathlib import Path
 from haisecret import MY_ID, MY_PW
-#전처리
+# 전처리
 from sheet2png import SHEETtoPNG
 from matplotlib import pyplot as plt
+# load_model
+import tensorflow as tf
+from io import BytesIO
 
-def main():
-    st.title("Handwriting Font Generation")
-    st.header("손글씨 폰트 생성")
-    st.markdown("**HAI 2023 project 3팀**")
-    st.subheader("")
-
-    # 이미지가 제공되는 웹 URL 또는 로컬 파일 시스템 경로
-    image_url_or_path = "./handwrite_sample.png"
-
-    # 이미지를 다운로드할 수 있는 링크 생성
-    download_link = create_download_link_from_url_or_path(image_url_or_path)
-
-    # "손글씨 작성 폼 다운로드" 문구 표시
-    st.subheader("손글씨 작성 폼 다운로드")
-    download = st.button("다운로드")
-    if download: st.markdown(download_link, unsafe_allow_html=True)
-    st.subheader("")
-
-    # "손글씨 폼 다운로드" 문구 표시
-    st.subheader("손글씨 이미지 등록")
-
-    # 사용자에게 이미지 파일을 업로드하도록 요청
-    uploaded_file = st.file_uploader("이미지 파일을 업로드하세요.", type=["jpg", "jpeg", "png"])
+@st.cache(show_spinner=False, allow_output_mutation=True, suppress_st_warning=True, max_entries=1)
+def load_model():
+    with st.spinner("Loading model for classification..."):
+        model_url = "" # github url
+        response = requests.get(model_url, allow_redirects=True)
+        model_content = BytesIO(response.content)
+        model = tf.keras.models.load_model(model_content)
     
-    # 사용자에게 완성된 폰트를 전송 받을 이메일 주소를 입력하도록 요청
-    email_address = st.text_input("폰트를 전송 받을 email 주소를 입력하세요.", value=None)
-    agree = False
-    if not email_address:
-        st.warning("email 주소를 입력해주세요.")
-    else:
-        agree = st.checkbox("개인 정보 제공에 동의합니다.")
-    
-    if agree:
-        save_path = ""
-        if uploaded_file is not None:
-            # 업로드된 이미지를 특정 폴더에 저장            
-            save_path = save_uploaded_image(uploaded_file, email_address)
-            st.success(f"이미지가 성공적으로 저장되었습니다.")
-
-        # 이미지 전처리 진행
-        # 또한 전처리된 이미지를 사이트에 표시
-        sheet_to_png(save_path, email_address) # save_path는 form 저장 위치
-    
-        #####################################
-        # Git에서 모델 불러와서 png파일 생성  #
-        # 그 후 여러가지 후처리              #
-        #####################################
-        ttf_file = False
-        ttf_file = "test.png" # ttf 파일로 수정해야함
-
-        # 사용자에게 완성된 폰트를 이메일로 전송
-        if(ttf_file):
-            send_mail(email_address, "나만의 손글씨 TTF 파일", "세상에 하나뿐인 나만의 손글씨 TTF 파일을 선물합니다!", ttf_file)
-            st.success(f"손글씨 파일이 성공적으로 전송되었습니다.")
+        return model
 
 def create_download_link_from_url_or_path(image_url_or_path):
     """
@@ -166,6 +124,63 @@ def send_mail(receiver, title, text, font=False):
     
     # 완료 메시지
     print(receiver, "성공", sep="\t")
+
+def main():
+    st.title("Handwriting Font Generation")
+    st.header("손글씨 폰트 생성")
+    st.markdown("**HAI 2023 project 3팀**")
+    st.subheader("")
+
+    model = load_model()
+
+    # 이미지가 제공되는 웹 URL 또는 로컬 파일 시스템 경로
+    image_url_or_path = "./handwrite_sample.png"
+
+    # 이미지를 다운로드할 수 있는 링크 생성
+    download_link = create_download_link_from_url_or_path(image_url_or_path)
+
+    # "손글씨 작성 폼 다운로드" 문구 표시
+    st.subheader("손글씨 작성 폼 다운로드")
+    download = st.button("다운로드")
+    if download: st.markdown(download_link, unsafe_allow_html=True)
+    st.subheader("")
+
+    # "손글씨 폼 다운로드" 문구 표시
+    st.subheader("손글씨 이미지 등록")
+
+    # 사용자에게 이미지 파일을 업로드하도록 요청
+    uploaded_file = st.file_uploader("이미지 파일을 업로드하세요.", type=["jpg", "jpeg", "png"])
+    
+    # 사용자에게 완성된 폰트를 전송 받을 이메일 주소를 입력하도록 요청
+    email_address = st.text_input("폰트를 전송 받을 email 주소를 입력하세요.", value=None)
+    agree = False
+    if not email_address:
+        st.warning("email 주소를 입력해주세요.")
+    else:
+        agree = st.checkbox("개인 정보 제공에 동의합니다.")
+    
+    if agree:
+        save_path = ""
+        if uploaded_file is not None:
+            # 업로드된 이미지를 특정 폴더에 저장            
+            save_path = save_uploaded_image(uploaded_file, email_address)
+            st.success(f"이미지가 성공적으로 저장되었습니다.")
+
+        # 이미지 전처리 진행
+        # 또한 전처리된 이미지를 사이트에 표시
+        sheet_to_png(save_path, email_address) # save_path는 form 저장 위치
+    
+        #####################################
+        # Git에서 모델 불러와서 png파일 생성  #
+        # 그 후 여러가지 후처리              #
+        #####################################
+        ttf_file = False
+        ttf_file = "test.png" # ttf 파일로 수정해야함
+
+        # 사용자에게 완성된 폰트를 이메일로 전송
+        if(ttf_file):
+            send_mail(email_address, "나만의 손글씨 TTF 파일", "세상에 하나뿐인 나만의 손글씨 TTF 파일을 선물합니다!", ttf_file)
+            st.success(f"손글씨 파일이 성공적으로 전송되었습니다.")
 
 if __name__ == "__main__":
     main()
